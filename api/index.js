61d6a09f5ace5ca553509eb028c4ca626b6a7bfb
Sathwikaerra <115003764+Sathwikaerra@users.noreply.github.com>
2024-04-06 11:15:53 +0530

    create user  model

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
 
 const app=express();
+
+mongoose.connect(process.env.MONGO).then(()=>console.log("DB connected successfully")).catch((err)=>{
+    console.log("error in DB connection ")
+})
 
 app.listen(3000,()=>{
     console.log("server running on port-3000")
