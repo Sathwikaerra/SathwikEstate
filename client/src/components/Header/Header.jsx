40ecac49853d76fbd0d223d97e9233a01c6e5074
Sathwikaerra <115003764+Sathwikaerra@users.noreply.github.com>
2024-04-10 15:35:18 +0530

    final5

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -40,12 +40,12 @@ useEffect(()=>{
   return (
    <header className='bg-slate-200 shadow-md'>
     <div className='flex items-center justify-between p-3 mx-auto max-w-6xl'>
-    <h1 className='text-sm sm:text-xl font-bold flex flex-wrap'>
+    <h1 className='text-sm sm:text-xl  font-bold flex flex-wrap'>
         <span className='text-slate-500 '>sathwik</span>
         <span className='text-slate-700'>Estate</span>
     </h1>
     <form  onSubmit={handleSubmit} className='bg-slate-100 rounded-lg p-2 flex items-center'>
-        <input  value={searchTerm}onChange={(e)=>setSearchTerm(e.target.value)}  type="text" placeholder='search...' className='bg-transparent focus:outline-none w-24 sm:w-64 ' />
+        <input  value={searchTerm}onChange={(e)=>setSearchTerm(e.target.value)}  type="text" placeholder='search...' className='bg-transparent focus:outline-none text-xs sm:text-xs w-14 sm:w-64 ' />
        
        
        <button>        <FaSearch className='text-slate-600' />
@@ -55,16 +55,16 @@ useEffect(()=>{
 
     <ul className='flex gap-4'>
         <Link to='/'>
-        <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+        <li className='text-xs sm:inline sm:text-xl text-slate-700 hover:text-blue-700'>Home</li>
        </Link>
        <Link to='/about'>
         
-        <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+        <li className='text-xs sm:inline sm:text-xl text-slate-700 hover:text-blue-700'>About</li>
         </Link>
 
        <Link to='/profile'>
         {
-            currentUser?( <img className='h-8 w-8 object-cover rounded-full' src={currentUser.avatar} alt="profile" /> ):(<li className='hidden sm:inline text-slate-700 hover:underline'>Sign In</li>)
+            currentUser?( <img className='h-8 w-8 object-cover rounded-full' src={currentUser.avatar} alt="profile" /> ):(<li className='text-xs sm:inline sm:text-xl text-slate-700 hover:text-blue-700'>Sign In</li>)
         }
         </Link>
     </ul>
